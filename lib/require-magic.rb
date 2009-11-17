a84b91fc8496c0a7946368b93653091569802da9
Kristian Mandrup <kmandrup@gmail.com>
2009-11-17 11:57:04 +0100

    tested ok

--- lib/require-magic.rb
+++ lib/require-magic.rb
@@ -1,29 +1,55 @@
 module Require
-  def self.folder(name, options)
+  def self.folder(name, options = {})
     includes_rexp = options[:include]
     excludes_rexp = options[:exclude]
     recursive = options[:recursive]
     folder_list = options[:folders]
+    file_pattern =  recursive ? "#{name}/**/*.rb" : "#{name}/*.rb" 
+    base_path = File.dirname(__FILE__)
+    path = File.join(base_path, file_pattern)
+    required_files = []
     
-    file_pattern =  recursive ? "#{name}/**/*.rb" : "#{name}/*.rb"    
-    Dir.glob(File.join(File.dirname(__FILE__), file_pattern)).each {|f| 
-      next if excludes &&  match(f, excludes_rexp)
-      require f if !match || match(f, includes_rexp)
+    Dir.glob(path).each {|f| 
+      next if excludes_rexp &&  match(f, excludes_rexp)
+      if !includes_rexp || match(f, includes_rexp)
+        required_files << do_require(f) 
+      end
     }
-    folders(folder_list, options) if folder_list
+    if folder_list
+      options.delete(:folders)     
+      folder_list.each do |folder|
+        sub_folder = File.join(name, folder)
+        required_files << folders(sub_folder, options) 
+      end
+    end
+    required_files.flatten
   end
 
-  def self.folders(folder_list, options)
-    folder_list.each(|f| folder(f, options))
+  def self.folders(folder_list, options = {})
+    required_files = []
+    folder_list.each do |f| 
+      required_files << folder(f, options)
+    end
+    required_files.flatten
   end
 
 protected
-  def match(f, rexp)
+  def self.do_require(name)
+    require name 
+    name
+  end
+
+  def self.match(f, rexp)
+    if rexp.kind_of? String
+      rexp = /#{Regexp.escape(rexp)}/
+    end
+      
     if rexp.kind_of? Array
       rexp.each{|e| return true if match(f, rexp)}
       false
-    elsif rexp.kind_of? RegExp  
-      f =~ rexp
+    elsif rexp.kind_of? Regexp  
+      match_res = (f =~ rexp)
+      !match_res.nil?
     end
   end
 
--- test/test_require_magic.rb
+++ test/test_require_magic.rb
@@ -1,1 +1,45 @@
-# TODO
+require 'require-magic'
+require "test/unit"
+ 
+class TestFilter < Test::Unit::TestCase
+
+  def setup
+    @folders = ['data', 'data2']
+  end
+
+  def teardown
+    ## Nothing really
+  end
+    
+  def test_require_folders
+    required_files = Require.folders(@folders)
+    found = !(required_files[0] =~ /data2\/blap_a.rb/).nil?
+    assert_equal true, found, "Should require blap_a.rb in /data" 
+  end
+
+  def test_require_folders_recursive
+    options = {:recursive => true}
+    required_files = Require.folders(@folders, options)
+    assert_equal 3, required_files.size, "Should require 3 files" 
+  end
+
+  def test_require_folders_recursive
+    options = {:recursive => true, :include => 'blip_'}
+    required_files = Require.folders(@folders, options)
+    assert_equal 2, required_files.size, "Should require 2 files" 
+  end
+
+  def test_require_folders_recursive
+    options = {:recursive => true, :exclude => 'blip_'}
+    required_files = Require.folders(@folders, options)
+    assert_equal 2, required_files.size, "Should require 2 file" 
+  end  
+
+  def test_require_folders_recursive
+    options = {:folders => ['blip']}
+    required_files = Require.folders(@folders, options)
+    assert_equal 3, required_files.size, "Should require 3 files" 
+  end  
+  
+end
+
